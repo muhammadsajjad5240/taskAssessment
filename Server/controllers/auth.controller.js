8dd3ce4df1cb77cf4e20fd77547738a4ce84b95b
Muhammad Sajjad <sajjadkhan42601@gmai.com>
2025-02-25 22:26:38 +0500

    Debugging Issue

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -4,8 +4,13 @@ const catchAsync = require('../utils/catchAsync');
 const { authService,  tokenService } = require('../services');
 
 const login = catchAsync(async (req, res) => {
+  console.log('start')
+
   const { email, password } = req.body;
+  console.log('req.body',req.body)
   const user = await authService.loginUserWithEmailAndPassword(email, password);
+  console.log('user',user)
+
   const tokens = await tokenService.generateAuthTokens(user);
   res.send({ user, tokens });
 });
